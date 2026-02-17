bb44a32a834feb5d5f8dc95f43410855dfcb8b29
antmanbatman786-alt <antmanbatman786@gmail.com>
2026-02-16 22:20:06 -0500

    Homework 2 for Intro to Python(with the Celcius portion of the code.

--- HW2.py
+++ HW2.py
@@ -16,3 +16,12 @@ print ('Your major , ', name)
 major = input()
 
 print('Hello, ', name, ', your address is ', address, ',phone number is ', p_number, ', and you major is ', major)
+
+
+print('Give me a temperature in Celcius: ')
+celcius = float(input())
+
+
+conversion = (9/5)* celcius + 32
+
+print ('This is the conversion from celcius to fahrenheit: ', conversion)

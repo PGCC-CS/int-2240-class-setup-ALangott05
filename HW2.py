90867b4177c5bfa848ef5aa3a9b1f3a4bf70d1c6
antmanbatman786-alt <antmanbatman786@gmail.com>
2026-02-16 19:05:00 -0500

    Homework 2 for Intro to Python

---
+++ HW2.py
@@ -0,0 +1,18 @@
+print ('Enter your last name : ')
+last_name = input()
+
+print ('Your last name is ',last_name)
+
+
+print ('Enter your name ')
+name = input()
+print ('What is your address, include ZIP code, city, and state  ', name)
+address = input()
+
+print ('What is your telephone number,',name)
+p_number = input()
+
+print ('Your major , ', name)
+major = input()
+
+print('Hello, ', name, ', your address is ', address, ',phone number is ', p_number, ', and you major is ', major)
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("something")
-print ("quality")
+print('Hello, what is your name')
+name = input()
+print('Hello, ' + name)
+
+
+num_1 = int(input('Enter a number: '))
+num_2 = int(input('Enter a number: '))
+
+
+result  = num_1 + num_2 /2
+
+print (num_1, '+' ,num_2, '=', result)
